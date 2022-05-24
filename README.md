10c502e969b51c7b29748802d2cc853654028384
Wisley Alves <wisley7lagoas@yahoo.com.br>
2022-05-24 12:24:36 -0300

    chore(Worker with R2): create new worker with R2

--- README.md
+++ README.md
@@ -1,53 +1,11 @@
 # PoC-storeApi 
 
 ## Cloudflare Workers
-This is my first worker.
 
-## Installing the Workers CLI
+## workers 
 
-https://developers.cloudflare.com/workers/
+## PoC Store-Api
+* e-com
 
-```npm install wrangler```
-
-## Test local
-
-- Run `npm run serve` 
-- Open a browser tab at http://localhost:8787/ to see my worker in action
-
-## Publish Worker
-
-- Run `npm run publish` to publish my worker
-- Open a browser tab at https://e-com.wisley.workers.dev/
-
-
-## Logs
-
-## Routes 
-
-
-### Store Api V1
-
-1) ```/product_v1```
-2) ```/categories_v1```
-3) ```/store_v1```
-4) ```/storeAll_v1```
-
-
-###  Store Api V2
-
-
-1) ```/product_v2```
-2) ```/categories_v2```
-3) ```/store_v2``` -->
-4) ```/storeAll_v2```
-
-## Reference 
-* Learn more at https://developers.cloudflare.com/workers/
-
-* https://developers.cloudflare.com/workers/get-started/guide/
-
-* https://github.com/cloudflare/worker-template-router/blob/master/index.js
-
-* https://egghead.io/lessons/cloudflare-make-api-requests-in-a-workers-function-using-the-fetch-api
-
-* https://github.com/cloudflare/wrangler-action
+## PoC worker with R2
+* e-com-cms
---
+++ e-com-cms/src/index.ts
@@ -0,0 +1,101 @@
+// https://developers.cloudflare.com/r2/examples/demo-worker/
+
+interface Env {
+  varName: string,
+  BUCKET: R2Bucket
+}
+
+type ResponseR2 = {
+  took: number | null
+  page: string | null
+}
+
+let optsHtml = {
+  headers: {
+    'Content-Type': 'text/html; charset=utf-8'
+    // 'Content-type': 'application/json',
+  },
+}
+
+let optsJson = {
+  headers: {
+    // 'Content-Type': 'text/html; charset=utf-8'
+    'Content-type': 'application/json',
+  },
+}
+
+const getR2 = async (file: string, env: Env): Promise<ResponseR2> => {
+  console.log('get R2: ', file)
+  const now = new Date().getTime();
+  const request = await env.BUCKET.get(file)
+  const result = await request?.text();
+  if (result) {
+    let response = {
+      took: (new Date().getTime() - now),
+      page: result,
+    }
+    return response
+  }
+  return {
+    took: null,
+    page: null
+  }
+
+}
+
+const getUrl = async (file: string): Promise<ResponseR2> => {
+  console.log('request URL: ', file)
+  const url = `https://${file}.netlify.app`
+  const now = new Date().getTime();
+  const getUrl = fetch(url)
+  const request = (await getUrl).text();
+  const result = await request;
+  let response = {
+    took: (new Date().getTime() - now),
+    page: result
+  }
+  return response;
+
+}
+
+export default {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    const url = new URL(request.url)
+    const paths = url.pathname.split('/');
+    const resource = paths[1];
+    const file = paths[2];
+    console.log(`\n method: ${request.method} \n resource: ${resource} \n url: ${request.url} \n file: ${file}`)
+
+
+    let took: number | null;
+    let req:string = 'url';
+    let page: string | null;
+    let response: ResponseR2;
+    //*
+    if (resource === 'r2') {
+      let now = new Date().getTime()
+      response = await getR2(`${file}.html`, env);
+      took = new Date().getTime() - now;
+      page = response.page;
+      req = 'r2'
+    } else {
+      let now = new Date().getTime()
+      response = await getUrl(file);
+      took = new Date().getTime() - now;
+      page = response.page;
+    }
+    let header = {
+      took,
+      page,
+      req,
+      file
+    }
+    // */
+    const headers = new Headers()
+    headers.set('took', `${response.took}`)
+    headers.set('Content-Type', 'text/html; charset=utf-8')
+    return new Response(response.page, {
+      headers,
+    })
+  },
+};
